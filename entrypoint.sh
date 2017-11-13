7bfa393818d9ecbecd44962621037531a04164b8
Anton Dzyk <anton.dzyk@yandex.ru>
2017-11-13 12:32:17 +0000

    FIX: run entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 VIDEO_SIZE=${VIDEO_SIZE:-"1920x1080"}
 DISPLAY=${DISPLAY:-"99"}
 FILE_NAME=${FILE_NAME:-"video.mp4"}
-exec ffmpeg -y -f x11grab -video_size "$VIDEO_SIZE" -r 12 -i "browser:$DISPLAY" -codec:v libx264 "/data/$FILE_NAME"
+exec ffmpeg -y -f x11grab -video_size $VIDEO_SIZE -r 12 -i browser:$DISPLAY -codec:v libx264 "/data/$FILE_NAME"
